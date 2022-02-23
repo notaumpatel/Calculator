d34354065a855302781aee26927ab3f9802c4c17
Aum Patel <aumrp17@gmail.com>
2022-02-23 21:17:22 +0530

    first

---
+++ script.js
@@ -0,0 +1,28 @@
+const $lis = document.querySelectorAll("ul li")
+
+$lis.forEach((node) => {
+    node.addEventListener('mousedown', function(event){
+        const value = node.innerText.trim()
+        const $result = document.querySelector(".result")
+        // $result.innerText = value
+        const resultText = $result.innerText.trim()
+
+        if(resultText == '0' || resultText == 'undefined' || resultText == 'Infinity'){
+            $result.innerText = ''
+        }
+
+        if(value == '='){
+            let solution = eval(resultText)
+            $result.innerText = solution
+            return true
+        }
+
+        if(value.toLowerCase() == 'c'){
+            $result.innerText = ''
+            return true
+        }
+
+        $result.append(value)
+
+    })
+})
